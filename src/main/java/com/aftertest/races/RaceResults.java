be38b601a6b454a097ed1bd35e17db5ac4f2b5c4
MKazakova <45536947+MKazakova@users.noreply.github.com>
2019-03-20 14:31:52 +0100

    Update RaceResults.java

--- src/main/java/com/aftertest/races/RaceResults.java
+++ src/main/java/com/aftertest/races/RaceResults.java
@@ -2,38 +2,47 @@ package com.aftertest.races;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 
 public class RaceResults {
-
-    private HashMap<String, Integer> results = new HashMap<>();
+    private HashMap<String, TimeDuration> results = new HashMap<>();
 
     public static void main(String ...args) {
         RaceResults rs = new RaceResults();
-        rs.onNewResult("1524", new TimeDuration(1200));
-        rs.onNewResult("1557", new TimeDuration(800));
-        rs.onNewResult("9000", new TimeDuration(1100));
+        rs.onNewResult("Tom Johnson", new TimeDuration(1200));
+        rs.onNewResult("Pamela Stone", new TimeDuration(800));
+        rs.onNewResult("Peter Stevenson", new TimeDuration(800));
+        rs.onNewResult("Roy Bowers", new TimeDuration(1100));
+        rs.onNewResult("Anne Tompson", new TimeDuration(970));
         rs.printResults();
     }
 
     public void onNewResult(String tagNumber, TimeDuration resultTime){
-        results.put(tagNumber, resultTime.getNbSeconds());
+        results.put(tagNumber, resultTime);
     }
 
     public void printResults(){
-        for (Entry<String, Integer> entry : results.entrySet()) {
-        	int nbWinners = 0;
-            if (entry.getValue()==Collections.min(results.values())) {
-                if(nbWinners==0) System.out.println("The winner is "+entry.getKey());
-                else System.out.println("Another winner is "+entry.getKey());
-                nbWinners++;
+    	TimeDuration  TimeMin = Collections.min(results.values(), (a, b)->a.getNbSeconds()-b.getNbSeconds());        	
+    	int nbWinners = (int)results.entrySet()
+                                        .stream()	
+                                        .filter(a->a.getValue().equals(TimeMin))
+                                        .count();
+
+        String affichage = nbWinners>1?"We have "+nbWinners+" winners :\n":"The winner is ";
+        System.out.print(affichage);
+        for (Entry<String, TimeDuration> entry : results.entrySet()) {
+
+            if (entry.getValue().equals(TimeMin)) {
+                System.out.println(entry.getKey()+" with a result of "+entry.getValue());
             }
         }
-        System.out.println("All the results : ");
+        System.out.println("The results of other participants : ");
         results.entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByValue())
+                .filter(a->!a.getValue().equals(TimeMin))
+                .sorted((t1, t2)->t1.getValue().getNbSeconds()-t2.getValue().getNbSeconds())
                 .forEach(System.out::println);
     }
+    
+
 }
