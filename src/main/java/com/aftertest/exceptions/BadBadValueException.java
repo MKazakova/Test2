4d54d6826d073bd4d951dbfb01975d84e8335cb7
MKazakova <45536947+MKazakova@users.noreply.github.com>
2019-03-21 17:39:17 +0100

    Update BadBadValueException.java

--- src/main/java/com/aftertest/exceptions/BadBadValueException.java
+++ src/main/java/com/aftertest/exceptions/BadBadValueException.java
@@ -1,5 +1,15 @@
 package com.aftertest.exceptions;
 
-public class BadBadValueException extends Throwable{
+public class BadBadValueException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+	
+	public BadBadValueException(String message) {
+		super(message);
+	}
+	
+	public BadBadValueException() {
+		super("Your time is negatif, it's too BAD!");
+	}
+	
 }
